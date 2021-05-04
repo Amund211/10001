153685adb14e161e2602f3ef9eeb67273d132efd
Amund Eggen Svandal <aesvandal@gmail.com>
2021-05-04 16:17:17 +0200

    Add tests and docstrings

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -4,12 +4,30 @@ from dice_10001.generate import generate_rolls
 
 
 def test_weightsum():
+    """Assert that the sum of the weights are correct"""
     for amt_dice in range(1, 7):
         weightsum = sum(weight for roll, weight in generate_rolls(amt_dice))
         assert weightsum == 6 ** amt_dice
 
 
-def test_unique_rolls():
+def test_amount_unique_rolls():
+    """Assert that the amount of unique rolls is correct"""
     for amt_dice in range(1, 7):
         amt_unique = sum(1 for roll, weight in generate_rolls(amt_dice))
         assert amt_unique == comb(6 + amt_dice - 1, amt_dice)
+
+
+def test_rolls_are_sorted():
+    """Assert that the rolls are sorted"""
+    for amt_dice in range(1, 7):
+        assert all(
+            tuple(sorted(roll)) == roll for roll, weight in generate_rolls(amt_dice)
+        )
+
+
+def test_rolls_are_unique():
+    """Assert that the rolls are unique"""
+    for amt_dice in range(1, 7):
+        amt_unique = len(set(roll for roll, weight in generate_rolls(amt_dice)))
+        amt = sum(1 for roll, weight in generate_rolls(amt_dice))
+        assert amt_unique == amt
